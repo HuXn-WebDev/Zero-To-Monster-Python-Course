525c0ba5ff123c4c7c76c014c29d6f38369e4331
HuXn WebDev <webdevacademy6@gmail.com>
2024-04-09 10:14:48 -0700

    Iterating Over Lists

---
+++ 2. Intermediate/7. Iterating Over Lists/index.py
@@ -0,0 +1,25 @@
+# ----------------------
+# for loop
+# my_list = [1, 2, 3, 4, 5]
+
+# for element in my_list:
+#     print(element)
+# ----------------------
+
+# ----------------------
+# Range & Length
+# my_list = [1, 2, 3, 4, 5]
+
+# for i in range(len(my_list)):
+#     print(my_list[i])
+# ----------------------
+
+# ----------------------
+#  While Loop
+my_list = [1, 2, 3, 4, 5]
+index = 0
+
+while index < len(my_list):
+    print(my_list[index])
+    index += 1
+# ----------------------
